883976d968a34a238fc6cf7f0165f7f970c19c89
Tobias Winkler <tobias.winkler@omio.com>
2026-07-22 14:26:03 +0200

    style(theme): adopt a restrained accessible palette

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -2,17 +2,17 @@ import { Platform } from "react-native";
 
 export const fonts = {
   display: Platform.select({
-    ios: "AvenirNext-DemiBold",
-    android: "sans-serif-condensed",
+    ios: "System",
+    android: "sans-serif-medium",
     default: undefined,
   }),
   displayHeavy: Platform.select({
-    ios: "AvenirNext-Heavy",
-    android: "sans-serif-condensed",
+    ios: "System",
+    android: "sans-serif-medium",
     default: undefined,
   }),
   body: Platform.select({
-    ios: "AvenirNext-Regular",
+    ios: "System",
     android: "sans-serif",
     default: undefined,
   }),
